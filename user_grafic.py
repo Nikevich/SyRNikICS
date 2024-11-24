81574adc4a695e772c03b5b822d1c676348b568c
Nikevich <nikshkilevich@yandex.ru>
2024-11-24 13:48:10 +0300

    То

--- user_grafic.py
+++ user_grafic.py
@@ -2,31 +2,61 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import math
 
-# Чтение данных
-data = pd.read_csv('processed/subs/4187.csv')  # Замените на путь к вашему файлу
+def getUpTx(path_subs):
+    data = pd.read_csv('processed/subs/' + path_subs)  # Замените на путь к вашему файлу
+    # Рассчитываем время начала каждой строки с учетом продолжительности
+    data['Timestamp'] = pd.to_datetime(data['StartSession']) + pd.to_timedelta(data['Duartion'], unit='s')
 
-# Рассчитываем время начала каждой строки с учетом продолжительности
-data['Timestamp'] = pd.to_datetime(data['StartSession']) + pd.to_timedelta(data['Duartion'], unit='s')
+    count = data.shape[0]
 
-count = data.shape[0]
+    for i in range(count - 1, 0, -1):  # Обратный цикл
+        if pd.isna(data.loc[i, 'EndSession']):
+            data.loc[i, 'DownTx'] = data.loc[i, 'DownTx'] - data.loc[i - 1, 'DownTx']
+        if data.loc[i, 'DownTx'] < 0:
+            data.loc[i, 'DownTx'] = data.loc[i, 'DownTx'] * (-1) 
 
-for i in range(count - 1, 0, -1):  # Обратный цикл
-    if pd.isna(data.loc[i, 'EndSession']):
-        data.loc[i, 'UpTx'] = data.loc[i, 'UpTx'] - data.loc[i - 1, 'UpTx']
+
+    # Строим график
+    plt.figure(figsize=(12, 6))
+
+    # Линия для DownTx
+    plt.plot(data['Timestamp'], data['DownTx'], 'r.')
+
+    # Настройки графика
+    plt.title('DownTx', fontsize=14)
+    plt.xlabel('Время', fontsize=12)
+    plt.ylabel('Трафик (bytes)', fontsize=12)
+    plt.grid(True)
+
+    plt.savefig("data/traffic_plot_UpTx.png")  # Сохраняем график в файл
+    return 'data/traffic_plot_UpTx.png'
+
+def getDownTx(path_subs):
+    data = pd.read_csv('processed/subs/' + path_subs)  # Замените на путь к вашему файлу
+    # Рассчитываем время начала каждой строки с учетом продолжительности
+    data['Timestamp'] = pd.to_datetime(data['StartSession']) + pd.to_timedelta(data['Duartion'], unit='s')
+
+    count = data.shape[0]
+
+    for i in range(count - 1, 0, -1):  # Обратный цикл
+        if pd.isna(data.loc[i, 'EndSession']):
+            data.loc[i, 'DownTx'] = data.loc[i, 'DownTx'] - data.loc[i - 1, 'DownTx']
+        if data.loc[i, 'DownTx'] < 0:
+            data.loc[i, 'DownTx'] = data.loc[i, 'DownTx'] * (-1) 
 
 
-# Строим график
-plt.figure(figsize=(12, 6))
+    # Строим график
+    plt.figure(figsize=(12, 6))
 
-# Линия для UpTx
-plt.plot(data['Timestamp'], data['UpTx'], 'b')
-# plt.plot(data['Timestamp'], data['DownTx'], 'r')
+    # Линия для DownTx
+    plt.plot(data['Timestamp'], data['DownTx'], 'r.')
 
-# Настройки графика
-plt.title('Использование трафика по времени', fontsize=14)
-plt.xlabel('Время', fontsize=12)
-plt.ylabel('Трафик (bytes)', fontsize=12)
-plt.grid(True)
+    # Настройки графика
+    plt.title('DownTx', fontsize=14)
+    plt.xlabel('Время', fontsize=12)
+    plt.ylabel('Трафик (bytes)', fontsize=12)
+    plt.grid(True)
 
-plt.savefig("traffic_plot.png")  # Сохраняем график в файл
+    plt.savefig("data/traffic_plot_DownTx.png")  # Сохраняем график в файл
+    return 'data/traffic_plot_DownTx.png'
 print("График сохранён как 'traffic_plot.png'")
